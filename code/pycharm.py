756056707742a55affcee6b40899aa56b8d6b9b1
Sven Hodapp <sven.hodapp@gmail.com>
2012-06-22 11:22:47 +0200

    Beispiel Python hinzugefuegt (Teil 1)

---
+++ code/pycharm.py
@@ -0,0 +1,33 @@
+import x as y
+from x import jo
+
+@decorator(param=1)
+def f(x):
+    """Syntax Highliting Demo
+
+    @param x Parameter
+
+    >>> f(123)
+    "test"
+
+    """
+
+    s = ("Test", 2+3, {'a': 'b'}, x)  # Comment
+    print s[0].lower()
+
+class Foo:
+    """Documentation of class Foo."""
+
+    def __init__(self):
+        self.sense = None
+        byte_string = 'newline:\n also newline:\x0a'
+        text_string = u"Cyrillic R is \u042f. Oops: \u042g"
+        f = Foo()
+        for i in f
+
+    def makeSense(self, whatever):
+        self.sense = whatever
+        map or file
+
+x = len('abc')
+print(f.__doc__)
